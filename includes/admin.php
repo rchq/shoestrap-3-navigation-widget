23e342e81bf1f09d5706f318dae89d3989caa91d
rchq <design@rchq.com>
2014-07-18 14:04:10 -0700

    Over hauled the code

--- includes/admin.php
+++ includes/admin.php
@@ -1,92 +1,103 @@
 <?php
+
+
 /*
  * Shoestrap 3 Extra Widget areas options
  */
-if ( !function_exists( 'shoestrap_nw_module_options' ) ) :
-function shoestrap_nw_module_options( $sections ) {
-
-
-	$section = array(
-		'title' => __( 'Navigation Widget', 'shoestrap_nw' ),
-		'icon'  => 'el-icon-check-empty  '
-	);
-
-    $fields[] = array(
-      "title"      => __("Display the Header.", "shoestrap_nw"),
-      "desc"      => __("Turn this ON to display the header. Default: OFF", "shoestrap"),
-      "id"        => "header_widget_toggle",
-      "customizer"=> array(),
-      "std"       => 0,
-      "type"      => "switch"
-    );
-		
-    $fields[] = array(
-      "title"      => __("Display branding on your Header.", "shoestrap_nw"),
-      "desc"      => __("Turn this ON to display branding (Sitename or Logo)on your Header. Default: ON", "shoestrap_nw"),
-      "id"        => "header_widget_branding",
-      "customizer"=> array(),
-      "std"       => 1,
-      "type"      => "switch",
-			"fold"      => "header_widget_toggle"
-    );
-
-    $fields[] = array(
-      "title"      => __("Header Background Color", "shoestrap_nw"),
-      "desc"      => __("Select the background color for your header. Default: #EEEEEE.", "shoestrap_nw"),
-      "id"        => "header_widget_bg",
-      "std"       => "#EEEEEE",
-      "customizer"=> array(),
-      "type"      => "color",
-			"fold"      => "header_widget_toggle"
-    );
-
-    $fields[] = array(
-      "title"      => __("Header Text Color", "shoestrap_nw"),
-      "desc"      => __("Select the text color for your header. Default: #333333.", "shoestrap_nw"),
-      "id"        => "header_widget_color",
-      "std"       => "#333333",
-      "customizer"=> array(),
-      "type"      => "color",
-			"fold"      => "header_widget_toggle"
-    );
-
-    $fields[] = array(
-      "title"      => __("Display the Header Margins.", "shoestrap_nw"),
-      "desc"      => __("Turn this ON to display the header margins. Default: OFF", "shoestrap"),
-      "id"        => "header_margin_widget_toggle",
-      "customizer"=> array(),
-      "std"       => 0,
-      "type"      => "switch"
-    );
-
-    $fields[] = array(
-      "title"      => __("Header Top Margin", "shoestrap_nw"),
-      "desc"      => __("Select the top margin of header in pixels. Default: 0px.", "shoestrap_nw"),
-      "id"        => "header_widget_margin_top",
-      "std"       => 1,
-      "min"       => 1,
-      "max"       => 200,
-      "type"      => "slider",
-			"fold"      => "header_margin_widget_toggle"
-    );
-
-    $fields[] = array(
-      "title"      => __("Header Bottom Margin", "shoestrap_nw"),
-      "desc"      => __("Select the bottom margin of header in pixels. Default: 0px.", "shoestrap_nw"),
-      "id"        => "header_widget_margin_bottom",
-      "std"       => 1,
-      "min"       => 1,
-      "max"       => 200,
-      "type"      => "slider",
-			"fold"      => "header_margin_widget_toggle"
-    );
-		
-	$section['fields'] = $fields;
-
-	$section = apply_filters( 'shoestrap_nw_module_options_modifier', $section );
+if ( !function_exists( 'shoestrap_nw_module_options' ) ) {
+	function shoestrap_nw_module_options( $sections ) {
 	
-	$sections[] = $section;
-	return $sections;
+	
+		$section = array(
+			'title' => __( 'Navigation Widget', 'shoestrap_nw' ),
+			'icon'  => 'el-icon-check-empty  '
+		);
+			if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+				$fields[] = array(
+					'title'			=> __('Cart Menu Item Toggle', 'shoestrap_hw'),
+					'desc'      	=> __('Turn this ON to display the woocommerce cart menu item in the navigation bar. Default: OFF', 'shoestrap_hw'),
+					'id'        	=> 'nav_widget_cart_toggle',
+					'default'     => 0,
+					'type'      	=> 'switch',
+				);
+			}
+	
+			$fields[] = array(
+				'title'      => __('Display the Header.', 'shoestrap_nw'),
+				'desc'      => __('Turn this ON to display the header. Default: OFF', 'shoestrap'),
+				'id'        => 'header_widget_toggle',
+				'customizer'=> array(),
+				'std'       => 0,
+				'type'      => 'switch'
+			);
+			
+			$fields[] = array(
+				'title'      => __('Display branding on your Header.', 'shoestrap_nw'),
+				'desc'      => __('Turn this ON to display branding (Sitename or Logo)on your Header. Default: ON', 'shoestrap_nw'),
+				'id'        => 'header_widget_branding',
+				'customizer'=> array(),
+				'std'       => 1,
+				'type'      => 'switch',
+				'fold'      => 'header_widget_toggle'
+			);
+	
+			$fields[] = array(
+				'title'      => __('Header Background Color', 'shoestrap_nw'),
+				'desc'      => __('Select the background color for your header. Default: #EEEEEE.', 'shoestrap_nw'),
+				'id'        => 'header_widget_bg',
+				'std'       => '#EEEEEE',
+				'customizer'=> array(),
+				'type'      => 'color',
+				'fold'      => 'header_widget_toggle'
+			);
+	
+			$fields[] = array(
+				'title'      => __('Header Text Color', 'shoestrap_nw'),
+				'desc'      => __('Select the text color for your header. Default: #333333.', 'shoestrap_nw'),
+				'id'        => 'header_widget_color',
+				'std'       => '#333333',
+				'customizer'=> array(),
+				'type'      => 'color',
+				'fold'      => 'header_widget_toggle'
+			);
+	
+			$fields[] = array(
+				'title'      => __('Display the Header Margins.', 'shoestrap_nw'),
+				'desc'      => __('Turn this ON to display the header margins. Default: OFF', 'shoestrap'),
+				'id'        => 'header_margin_widget_toggle',
+				'customizer'=> array(),
+				'std'       => 0,
+				'type'      => 'switch'
+			);
+	
+			$fields[] = array(
+				'title'      => __('Header Top Margin', 'shoestrap_nw'),
+				'desc'      => __('Select the top margin of header in pixels. Default: 0px.', 'shoestrap_nw'),
+				'id'        => 'header_widget_margin_top',
+				'std'       => 1,
+				'min'       => 1,
+				'max'       => 200,
+				'type'      => 'slider',
+				'fold'      => 'header_margin_widget_toggle'
+			);
+	
+			$fields[] = array(
+				'title'      => __('Header Bottom Margin', 'shoestrap_nw'),
+				'desc'      => __('Select the bottom margin of header in pixels. Default: 0px.', 'shoestrap_nw'),
+				'id'        => 'header_widget_margin_bottom',
+				'std'       => 1,
+				'min'       => 1,
+				'max'       => 200,
+				'type'      => 'slider',
+				'fold'      => 'header_margin_widget_toggle'
+			);
+			
+		$section['fields'] = $fields;
+	
+		$section = apply_filters( 'shoestrap_nw_module_options_modifier', $section );
+		
+		$sections[] = $section;
+		return $sections;
+	}
+	add_filter( 'redux/options/' . REDUX_OPT_NAME . '/sections', 'shoestrap_nw_module_options' );
 }
-add_filter( 'redux/options/' . REDUX_OPT_NAME . '/sections', 'shoestrap_nw_module_options' );
-endif;
--- includes/functions.php
+++ includes/functions.php
@@ -23,36 +23,96 @@ class jb_navigation_widget extends WP_Widget {
 	 * @param array $args     Widget arguments.
 	 * @param array $instance Saved values from database.
 	 */
-	public function widget( $args, $instance ) {
+	public function widget($args, $instance) {
 		global $ss_settings;
-
-		echo $args['before_widget'];
-		if ( ! has_action( 'shoestrap_navigation_top_navbar_override' ) ) : ?>
-      <nav class="navbar-inverse" role="navigation">
-        <div class="container">
-          <?php global $woocommerce; ?>
-          <ul class="nav nav-pills">
-            <li>
-              <a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>">
-                <i class="glyphicon glyphicon-shopping-cart"></i> <?php echo sprintf(_n('(%d)', '(%d)', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?>
-              </a>
-            </li>
-            <?php
-              if (has_nav_menu('primary_navigation')) :
-                wp_nav_menu(array( 'container' => false, 'items_wrap' => '%3$s', 'theme_location' => 'tertiary_navigation', 'menu_class' => ''));
-              endif;
-            ?>
-          </ul>		
-        </div>		
-      </nav>
-		<?php endif;
-		echo $args['after_widget'];
+		// Get menu
+    $nav_menu = ! empty( $instance['nav_menu'] ) ? wp_get_nav_menu_object( $instance['nav_menu'] ) : false;
+		
+    if ( !$nav_menu ) {
+      return;
+		} else {
+			echo $args['before_widget'];
+			if ( ! has_action( 'shoestrap_navigation_top_navbar_override' ) ) {
+			?>
+				<nav class="navbar-inverse" role="navigation">
+					<div class="<?php echo apply_filters( 'shoestrap_navbar_container_class', 'container' ); ?>">
+          	<ul class="nav nav-pills">
+              <?php 
+							do_action( 'jb_pre_nav_menu_item' );
+							wp_nav_menu( array( 'menu' => $nav_menu, 'container' => false, 'items_wrap' => '%3$s', 'fallback_cb' => false, 'menu_class' => '' ) );
+              do_action( 'jb_post_nav_menu_item' );
+							?>
+          	</ul>		
+        	</div>		
+      	</nav>
+      <?php
+			}
+			echo $args['after_widget'];
+		}
+		
+  }
+	
+	function form($instance) {
+		
+		$menus = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
+		 
+		// If no menus exists, direct the user to go and create some.
+		if ( !$menus ) {
+			echo '<p>'. sprintf( __('No menus have been created yet. <a href="%s">Create some</a>.'), admin_url('nav-menus.php') ) .'</p>';
+			return;
+		} 
+		?>
+					
+		<p>
+			<label for="<?php echo $this->get_field_id('nav_menu'); ?>"><?php _e('Select Menu:'); ?></label>
+			<select id="<?php echo $this->get_field_id('nav_menu'); ?>" name="<?php echo $this->get_field_name('nav_menu'); ?>">
+				<?php
+        foreach ( $menus as $menu ) {
+					$selected = $nav_menu == $menu->term_id ? ' selected="selected"' : '';
+					echo '<option'. $selected .' value="'. $menu->term_id .'">'. $menu->name .'</option>';
+				}
+				?>
+			</select>
+		</p>
+        
+	<?php    
 	}
+	
+	function update( $new_instance, $old_instance ) {
+		
+    $instance['nav_menu'] = (int) $new_instance['nav_menu'];
+    return $instance;
+		
+  }
 
 } // class jb_navigation_widget
 
-if ( ! function_exists( 'woocommerce_template_single_meta' ) ) :
-	function woocommerce_template_single_meta() {
-		wc_get_template( 'lib/cart.php' );
+
+// register jb_navigation_widget widget
+function jb_navigation_widget_register() {
+    register_widget( 'jb_navigation_widget' );
+}
+add_action( 'widgets_init', 'jb_navigation_widget_register' );
+
+// add shopping cart menu item to navigation
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) { //check if woocommerce is active
+	if ( ! function_exists( 'jb_wc_cart_menu_item' ) ) {
+		function jb_wc_cart_menu_item(){ 
+			global $woocommerce;
+			global $ss_settings;
+			
+			if ( shoestrap_getVariable( 'nav_widget_cart_toggle' ) == 1 ) {
+			?>
+			
+				<li>
+					<a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>">
+						<i class="glyphicon glyphicon-shopping-cart"></i> <?php echo sprintf(_n('(%d)', '(%d)', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?>
+					</a>
+				</li>
+			
+			<?php
+			}
+		}
 	}
-endif;
+	add_action( 'jb_pre_nav_menu_item', 'jb_wc_cart_menu_item', 10 );
+}// end check if woocommerce is active
